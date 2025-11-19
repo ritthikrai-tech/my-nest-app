a3011ba5f5e5f2543f40b373182ce72e769e3382
YKK <yamkoko18@gmail.com>
2025-11-19 15:09:39 +0700

    Refactor e2e test formatting to use single quotes and streamline request assertions

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import type { INestApplication } from "@nestjs/common";
-import { Test, type TestingModule } from "@nestjs/testing";
-import { AppModule } from "../src/modules/app/app.module";
-import request from "supertest";
-import type { App } from "supertest/types";
+import type { INestApplication } from '@nestjs/common';
+import { Test, type TestingModule } from '@nestjs/testing';
+import { AppModule } from '../src/modules/app/app.module';
+import request from 'supertest';
+import type { App } from 'supertest/types';
 
-describe("AppController (e2e)", () => {
+describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -16,10 +16,7 @@ describe("AppController (e2e)", () => {
     await app.init();
   });
 
-  it("/ (GET)", () => {
-    return request(app.getHttpServer())
-      .get("/health")
-      .expect(200)
-      .expect({ status: "ok" });
+  it('/ (GET)', () => {
+    return request(app.getHttpServer()).get('/health').expect(200).expect({ status: 'ok' });
   });
 });
