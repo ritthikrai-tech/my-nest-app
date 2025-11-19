4e70deee9f3333f599f5a5f0cfea1a4e73785a59
YKK <yamkoko18@gmail.com>
2025-11-19 15:04:31 +0700

    refactor format code in main file

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,24 @@
-import { Logger } from "@nestjs/common";
-import { NestFactory } from "@nestjs/core";
-import {
-  DocumentBuilder,
-  type SwaggerCustomOptions,
-  SwaggerModule,
-} from "@nestjs/swagger";
-import { cleanupOpenApiDoc } from "nestjs-zod";
-import { AppModule } from "./modules/app/app.module";
-import { SwaggerTheme, SwaggerThemeNameEnum } from "swagger-themes";
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, type SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
+import { cleanupOpenApiDoc } from 'nestjs-zod';
+import { AppModule } from './modules/app/app.module';
+import { SwaggerTheme, SwaggerThemeNameEnum } from 'swagger-themes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle("My NestJS API example")
-    .setDescription("The My NestJS API description")
-    .setVersion("1.0")
+    .setTitle('My NestJS API example')
+    .setDescription('The My NestJS API description')
+    .setVersion('1.0')
     .build();
   const documentFactory = SwaggerModule.createDocument(app, config, {});
   const theme = new SwaggerTheme();
   const options: SwaggerCustomOptions = {
     customCss: theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK),
   };
-  SwaggerModule.setup("docs", app, cleanupOpenApiDoc(documentFactory), options);
+  SwaggerModule.setup('docs', app, cleanupOpenApiDoc(documentFactory), options);
 
   const port = process.env.PORT ?? 3000;
 
